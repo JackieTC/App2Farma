a3ebfb4751b3231cfddc722367e8babd8a04ed65
JackieTC <jackeline15tc@gmail.com>
2016-01-12 01:48:31 -0500

    Revert "App2Farma"
    This reverts commit a18d358ba4568f5209f01e3750a8cecd477c03c3.

--- webservice/configuracion.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('db_servidor','localhost');
-define('db_usuario','root');
-define('db_clave','');
-define('db_nombre','fullfarma');
--- webservice/obtener-impuestos-y-moneda.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-type: application/json');
-?>
-{"data":[{"tax_n_currency":{"Variable":"Tax","Value":"0"}},{"tax_n_currency":{"Variable":"Currency","Value":"SOL"}}]}
